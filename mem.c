0e23b53dfd6332718040d218ec77c5c73a99950b
Dale Weiler <killfieldengine@gmail.com>
2012-12-01 01:57:03 +0000

    Mitigate to individual system allocations

--- mem.c
+++ mem.c
@@ -21,299 +21,222 @@
  * SOFTWARE.
  */
 #include "gmqcc.h"
+#define MEM_HEAPS      32
+#define MEM_HEAPLINE   24             /* line MEM_HEAP is defined on above     */
+#define MEM_ALLOC(X)   malloc (X)     /* internal allocation function to use   */
+#define MEM_FREE(X)    free   (X)     /* internal deallocation function to use */
 
-/*
- * inlined utility macros, O(1) lookup for size,links and next
- * block addresses.
- */
-#define GMQCC_MEM_BLOCKSIZE(X,Y) ((X)->data[(Y)])
-#define GMQCC_MEM_BLOCKNEXT(X,Y) ((X)->data[(Y) + (X)->data[(Y)]])
-#define GMQCC_MEM_BLOCKLINK(X,Y) ((Y)           + (X)->data[(Y)])
+static mem_heap_t *mem_heap_data[MEM_HEAPS];
+static size_t      mem_heap_size = 0;
 
-mem_heap_t *mem_init(size_t size, const char *desc, const char *file, size_t line) {
-    void       *data = malloc(sizeof(mem_heap_t) + size);
-    mem_heap_t *heap = (mem_heap_t*)data;
-    
-    heap->data                 = (int*)(((unsigned char *)data) + sizeof(mem_heap_t));
-    heap->size                 = size / sizeof(int);
-    heap->left                 = 0;
-    heap->data[0]              = heap->size - 1;
-    heap->data[heap->size - 1] = heap->size;
-    heap->file                 = file;
-    heap->desc                 = desc;
-    heap->line                 = line;
-    heap->info                 = NULL;
-    heap->allocations          = 0;
-    heap->deallocations        = 0;
-    heap->allocated            = 0;
-    heap->deallocated          = 0;
-    
+mem_heap_t *_mem_new(const char *name, const char *file, const char *func, size_t line) {
+    mem_heap_t *heap = NULL;
+
+    /* allocate the data */
+    if (!mem_heap_size) {
+        size_t i = 0;
+        for (; i < sizeof(mem_heap_data) / sizeof (*mem_heap_data); i++)
+            mem_heap_data[i] = (mem_heap_t*) MEM_ALLOC (sizeof(mem_heap_t));
+    }
+
+    /* increase MAX_HEAP .. not enough stack space for another */
+    if (mem_heap_size + 1 >= sizeof(mem_heap_data) / sizeof(*mem_heap_data)) {
+        con_err("Too many heaps, increase MEM_HEAPS, defined at %s:%d\n", __FILE__, MEM_HEAPLINE);
+        abort();
+    }
+
+    heap       = mem_heap_data[mem_heap_size ++];
+    heap->name = name;
+    heap->func = func;
+    heap->file = file;
+    heap->line = line;
+    heap->info = NULL;
+
     return heap;
 }
 
-void mem_destroy(mem_heap_t *heap, const char *file, size_t line) {
-    void *base = NULL;
-    
-    util_debug("MEM", "Heap [%s] destroyed at: %s:%d\n",
-        heap->desc, file, line);
-        
-    /* get the base pointer used for malloc */
-    base = (void*)(((unsigned char *)heap->data) - sizeof(mem_heap_t));
-    free(base);
-}
+void *_mem_alloc(mem_heap_t *heap, size_t size, const char *file, const char *func, size_t line) {
+    mem_info_t *info = (mem_info_t*) MEM_ALLOC (sizeof(mem_info_t) + size);
+    void       *data = (void*)(info + 1);
 
-void *mem_alloc(mem_heap_t *heap, size_t size, const char *file, size_t line) {
-    size_t       find;
-    size_t       self;
-    size_t       prev;
-    bool         stop  = true;
-    mem_block_t *block = NULL;
-    
-    if (size == 0)
-        return NULL;
-        
-    /* find correct size  */
-    size += sizeof(mem_block_t);
-    find  = size / sizeof(int);
-    if     (size % sizeof(int) > 0) find++;
-    
-    /* allocate */
-    prev = heap->left;
-    self = heap->left;
-    
-    /* TODO: optimize */
-    mem_alloc_test: {
-        /* out of memory for heap? */
-        if (heap->left == heap->size)
-            return NULL;
-        
-        /* block size too small? */
-        if ((size_t)GMQCC_MEM_BLOCKSIZE(heap, self) < find) {
-            stop = false;
-            prev = self;
-            
-            if ((size_t)GMQCC_MEM_BLOCKNEXT(heap, self) == heap->size)
-                return NULL;
-            else
-                self = GMQCC_MEM_BLOCKNEXT(heap, self);
-                
-            /* retry */
-            goto mem_alloc_test;
-        }
-        
-        /* perfect fit :-) */
-        if ((size_t)GMQCC_MEM_BLOCKSIZE(heap, self) == find) {
-            if (stop)
-                heap->left = GMQCC_MEM_BLOCKNEXT(heap, self);
-            else
-                heap->data[GMQCC_MEM_BLOCKLINK(heap, prev)] = GMQCC_MEM_BLOCKNEXT(heap, self);
-        } else {
-            /* not so perfect (size is larger) */
-            if (stop) {
-                heap->left =
-                    (((size_t)GMQCC_MEM_BLOCKSIZE(heap, self) - find) == 1) ?
-                        (size_t)GMQCC_MEM_BLOCKNEXT(heap, self) : self + find + 1;
-            } else {
-                heap->data [GMQCC_MEM_BLOCKLINK(heap, prev)] =
-                    (((size_t)GMQCC_MEM_BLOCKSIZE(heap, self) - find) == 1) ?
-                        (size_t)GMQCC_MEM_BLOCKNEXT(heap, self) : self + find + 1;
-            }
-            
-            /* final */
-            if (((size_t)GMQCC_MEM_BLOCKSIZE(heap, self) - find) == 1)
-                heap->data[self] = find + 1;
-            else
-                heap->data[self + find + 1] = GMQCC_MEM_BLOCKSIZE(heap, self) - find - 1,
-                heap->data[self]            = find;
+    if (!info) return NULL;
 
-        }
-    }
-    heap->allocations ++;
-    heap->allocated   += size - sizeof(mem_block_t);
-    
-    /* 
-     * store the "actual" size, line, and file
-     * into the data for debug.
-     */
-    block       = (mem_block_t*) &heap->data[self + 1];
-    block->size = size - sizeof(mem_block_t);
-    block->line = line;
-    block->file = file;
-    block->prev = NULL;
-    block->next = heap->info;
-    
+
+    info->func = func;
+    info->file = file;
+    info->line = line;
+    info->size = size;
+    info->prev = NULL;
+    info->next = heap->info;
+    info->heap = (void*)heap;
+
     if (heap->info)
-        heap->info->prev = block;
-    heap->info = block;
-    
-    util_debug("MEM", "allocation:   % 8u (bytes) address 0x%08X @ %s:%u\n",
-        block->size,
-        (void*)(block + 1),
-        block->file,
-        block->line
-    );
-    
-    return (void*) (block + 1);
+        heap->info->prev = info;
+
+    heap->info        = info;
+    heap->allocated  += size;
+    heap->allocations++;
+
+    return data;
 }
 
-void mem_free(mem_heap_t *heap, void *ptr, const char *file, size_t line) {
-    mem_block_t *block = ((mem_block_t*)ptr) - 1;
-    int          free  = ((int *)block - (heap->data + 1));
-    size_t       size  = block->size;
-    size_t       self;
-    size_t       prev;
-    
-    util_debug("MEM", "released:     % 8u (bytes) address 0x%08X @ %s:%u\n",
-        block->size,
-        ptr,
-        file,
-        line
-    );
-    
-    /* rollback blocks */
-    if (block->prev)
-        block->prev->next = block->next;
-    if (block->next)
-        block->next->prev = block->prev;
-    
-    /* start at next ? */
-    if (block == heap->info)
-        heap->info = block->next;
-    
-    /* before first free block? */
-    if ((size_t)free < heap->left) {
-        /* immediately after block being freed? */
-        if ((((size_t)GMQCC_MEM_BLOCKLINK(heap, free) + 1) == heap->left) && heap->left < heap->size)
-            heap->data[free] += (heap->data[heap->left] + 1);
-        else
-            heap->data[GMQCC_MEM_BLOCKLINK(heap, free)] = heap->left;
-        /* update availble amount */
-        heap->left = free;
-    } else {
-        /* isn't before first free block? */
-        prev = self = heap->left;
-        
-        /* TODO: optimize */
-        while (self < (size_t)free) {
-            prev = self;
-            self = GMQCC_MEM_BLOCKNEXT(heap, self);
-        }
-        
-        /* immediately before block being freed? */
-        if ((GMQCC_MEM_BLOCKLINK(heap, prev) + 1) == (size_t)free)  {
-            /* deframentation of free space */
-            heap->data[prev] += (heap->data[free] + 1);
-            /* immediately after block being freed? */
-            if ((((size_t)GMQCC_MEM_BLOCKLINK(heap, free) + 1) == self) && self < heap->size) {
-                /* defragmentation of free space */
-                heap->data[prev] += (heap->data[self] + 1);
-            } else {
-                heap->data[GMQCC_MEM_BLOCKLINK(heap, free)] = self;
-            }
-        } else {
-            heap->data[GMQCC_MEM_BLOCKLINK(heap, prev)] = free;
-            heap->data[GMQCC_MEM_BLOCKLINK(heap, free)] = self;
-        }
+/* don't need the heap as argument */
+void _mem_free(mem_heap_t *heap, void *data, const char *file, const char *func, size_t line) {
+    mem_info_t *info = NULL;
+    if (!data) return;
+
+    info = ((mem_info_t*)data - 1);
+
+    /* handle mismatch, but warn for it */
+    if (heap != (mem_heap_t*)info->heap) {
+        util_debug("MEM", "Warning: mismatched free. Allocated from heap: %s, attempted free from heap: %s [%s:%s:%d]\n",
+            heap->name, ((mem_heap_t*)info->heap)->name,
+            file, func, line
+        );
+
+        /* redirect */
+        heap = (mem_heap_t*)info->heap;
     }
+
+    heap->deallocated   += info->size;
     heap->deallocations ++;
-    heap->deallocated   += size;
+
+    if (info->prev) info->prev->next = info->next;
+    if (info->next) info->next->prev = info->prev;
+
+    if (info == heap->info)
+        heap->info = info->next;
+
+    MEM_FREE (info);
 }
 
 /*
- * TODO: optimize by inlining the alloc/free as duped code eliminating
- * unneeded logic.
- */
-void *mem_realloc(mem_heap_t *heap, void *ptr, size_t size, const char *file, size_t line) {        
-    void        *new = mem_alloc(heap, size, file, line);
-    mem_block_t *old;
-    
-    if (!ptr)
-        return new;
+ * Easily obtain memory size for things like _vec (no need to duplicate
+ * data for this).
+ */ 
+size_t mem_size(void *data) {
+    return ((mem_info_t*)data - 1)->size;
+}
+
+void *_mem_realloc(mem_heap_t *heap, void *data, size_t size, const char *file, const char *func, size_t line) {
+    mem_info_t *oldinfo = NULL;
+    mem_info_t *newinfo = NULL;
+
+    if (!data)
+        return _mem_alloc(heap, size, file, func, line);
     if (!size) {
-        mem_free(heap, ptr, file, line);
+        _mem_free(heap, data, file, func, line);
         return NULL;
     }
-        
-    old = ((mem_block_t*)ptr) - 1;
-    
-    util_debug("MEM", "reallocating: % 8u (bytes) address 0x%08X @ %s:%u to %d (bytes)\n",
-        old->size,
-        ptr,
-        old->file,
-        old->line,
-        size
-    );
-    memcpy(new, ptr, old->size);
-    mem_free(heap, ptr, file, line);
-    return new;
-}
 
-/*
- * TODO: mutual exclusion for threadsafety if our compiler decides
- * to go multithreaded.
- * 
- * Note: we cannot use a vector for this vector requires mem_a
- * so .. to combat this we just use a fixed size.  Since we know
- * statically how many we need.
- */
-static mem_heap_t *mem_heaps[1] = { NULL };
-static size_t      mem_bumps    = 0;
-mem_heap_t *mem_heap_add(const char *desc, size_t size, const char *file, size_t line) {
-    mem_heap_t  *heap    = mem_init(size, desc, file, line);
-    mem_heaps[mem_bumps] = heap;
-    util_debug("MEM", "Added new heap: %s, %lu (bytes)\n",
-        desc, size);
-    mem_bumps++;
-    return heap;
-}
+    oldinfo = ((mem_info_t*)data - 1);
+    newinfo = ((mem_info_t*) MEM_ALLOC (sizeof(mem_info_t) + size));
 
-void mem_heap_dump(mem_heap_t *heap) {
-    mem_block_t *block = heap->info;
-    
-    util_debug("MEM", "Heap Report:\n");
-    util_debug("MEM", "    Description: %s\n", heap->desc);
-    util_debug("MEM", "    Initialized: %s:%d\n", heap->file, heap->line);
-    util_debug("MEM", "    Size:        %d (bytes)\n", heap->size * sizeof(int));
-    
-    while (block) {
-        util_debug("MEM", "    Lost: % 8u (bytes) at %s:%u\n",
-            block->size,
-            block->file,
-            block->line
-        );
-        
-        block = block->next;
+    if (!newinfo) {
+        _mem_free(heap, oldinfo + 1, file, func, line);
+        return NULL;
     }
-    
-    util_debug("MEM", "    Memory Report:\n");
-    util_debug("MEM", "        Total allocations:   %llu\n", heap->allocations);
-    util_debug("MEM", "        Total deallocations: %llu\n", heap->deallocations);
-    util_debug("MEM", "        Total allocated:     %llu (bytes)\n", heap->allocated);
-    util_debug("MEM", "        Total deallocated:   %llu (bytes)\n", heap->deallocated);
-    util_debug("MEM", "\n");
-    util_debug("MEM", "        Leaks found:\n");
-    util_debug("MEM", "            Lost: %llu (bytes)\n", (heap->allocated   - heap->deallocated));
-    util_debug("MEM", "            From: %d (alllocations)\n",  (heap->allocations - heap->deallocations));
+
+    memcpy(newinfo+1, oldinfo+1, oldinfo->size);
+
+    if (oldinfo->prev) oldinfo->prev->next = oldinfo->next;
+    if (oldinfo->next) oldinfo->next->prev = oldinfo->prev;
+
+    if (oldinfo == heap->info)
+        heap->info = oldinfo->next;
+
+    newinfo->func = func;
+    newinfo->file = file;
+    newinfo->line = line;
+    newinfo->size = size;
+    newinfo->prev = NULL;
+    newinfo->next = heap->info;
+    newinfo->heap = (void*)heap;
+
+    if (heap->info)
+        heap->info->prev = newinfo;
+    heap->info = newinfo;
+
+    heap->allocated -= oldinfo->size;
+    heap->allocated += newinfo->size;
+
+    MEM_FREE (oldinfo);
+
+    return newinfo + 1;
 }
 
-/*
- * Cleverly dumps all information for all heaps with basic
- * iteration.  Since mem_heaps is only a vector (for now).
- */
+void _mem_freeall(mem_heap_t *heap, const char *file, const char *func, size_t line) {
+    mem_info_t *info;
+    for (info = heap->info; info; info = info->next)
+        _mem_free (heap, info + 1, file, func, line);
+}
+
+void mem_destroy() {
+    size_t i = 0;
+    for (; i < mem_heap_size; i++)
+        MEM_FREE (mem_heap_data[i]);
+}
+
 void mem_dump() {
-    size_t select = 0;
-    for (; select < mem_bumps; select++) {
-        mem_heap_dump(mem_heaps[select]);
+    mem_info_t *info          = NULL;
+    mem_heap_t *heap          = NULL;
+    const char *bestlargename = NULL;
+    const char *bestsmallname = NULL;
+    uint64_t    bestlarge     = 0;
+    uint64_t    bestsmall     = 0;
+    size_t      next          = 0;
+
+    /* Find largest heap */
+    for (next = 0, heap = *mem_heap_data; next < mem_heap_size; heap = mem_heap_data[next]) {
+        if (heap->allocated > bestlarge) {
+            bestlarge     = heap->allocated;
+            bestsmall     = bestlarge;
+            bestlargename = heap->name;
+            bestsmallname = bestlargename;
+        }
+        next++;
+    }
+
+    /* Find smallest heap */
+    for (next = 0, heap = *mem_heap_data; next < mem_heap_size; heap = mem_heap_data[next]) {
+        if (heap->allocated < bestsmall) {
+            bestsmall     = heap->allocated;
+            bestsmallname = heap->name;
+        }
+        next++;
+    }
+
+            
+    util_debug("MEM", "Memory Report:\n");
+    util_debug("MEM", "    Heaps:    %u\n", mem_heap_size);
+    util_debug("MEM", "    Largest:  %s: %llu (bytes)\n", bestlargename, bestlarge);
+    util_debug("MEM", "    Smallest: %s: %llu (bytes)\n", bestsmallname, bestsmall);
+    util_debug("MEM", "\n");
+    for (next = 0, heap = *mem_heap_data; next < mem_heap_size; heap = mem_heap_data[next]) {
+        /* Heap Report: */
+        util_debug("MEM", "    Heap Report:\n");
+        util_debug("MEM", "        Name:                %s\n"           , heap->name);
+        util_debug("MEM", "        Initializated At:    %s:%u in %s\n"  , heap->file, heap->line, heap->func);
+        util_debug("MEM", "        Total Allocations:   %llu\n"         , heap->allocations);
+        util_debug("MEM", "        Total Deallocations: %llu\n"         , heap->deallocations);
+        util_debug("MEM", "        Total Allocated:     %llu (bytes)\n" , heap->allocated);
+        util_debug("MEM", "        Total Deallocated:   %llu (bytes)\n" , heap->deallocated);
+        util_debug("MEM", "        Leak Report:\n");
+        util_debug("MEM", "            Lost: %llu (bytes)\n"      , heap->allocated   - heap->deallocated);
+        util_debug("MEM", "            From: %llu (allocations)\n", heap->allocations - heap->deallocations);
+
+        /* there was leaks */
+        if (heap->allocated - heap->deallocated > 0) {
+            util_debug("MEM", "            Leak Traceback:\n");
+            for (info = heap->info; info; info = info->next) {
+                util_debug("MEM", "                Lost: % 8u (bytes) at %s:%u in %s\n",
+                    info->size,
+                    info->file,
+                    info->line,
+                    info->func
+                );
+            }
+        }
+        next++;
     }
 }
-
-void mem_destroyall(const char *file, size_t line) {
-    size_t select = 0;
-    for (; select < mem_bumps; select++) {
-        mem_destroy(mem_heaps[select], file, line);
-    }
-}
-
-#undef GMQCC_MEM_BLOCKLINK
-#undef GMQCC_MEM_BLOCKNEXT
-#undef GMQCC_MEM_BLOCKSIZE
